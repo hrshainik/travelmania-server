39cdd20fb6c15ac214a1a94cb546cf597ef62ad2
hrshainik <hrsshainik@gmail.com>
2021-08-13 12:33:57 +0600

    data sanitization & prevent parameter pollution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorControllers");
@@ -10,20 +13,46 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+// Set secure HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit request form same ip
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many request from this ip address, please try again later."
 });
-
-//  apply to all requests
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, parse data from req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price"
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
